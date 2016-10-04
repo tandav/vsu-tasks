87f870d5c7e2b6d40b9220867ce13c2e47e32b8b
Alexander Rodionov <alx1dr@gmail.com>
2016-10-04 14:19:44 +0300

    Update math-modeling.py

--- math-modeling.py
+++ math-modeling.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-omega = 1
-k = 1
-h = 0.1
+omega = 3
+k = 0.
+h = 0.01
+
+ang0 = 0
+ang_vel0 = 8
+t_r = 50
+
 
 def f1(y, z):
     return z
@@ -11,6 +16,13 @@ def f1(y, z):
 def f2(y, z):
     return -omega * np.sin(y) - k*z
 
+def simp_sin(t):
+    C1, C2 = ang_vel0, ang0
+    return C1/omega * np.sin(t*omega) + C2 * np.cos(t*omega)
+
+def dsimp_sin(t):
+    C1, C2 = ang_vel0, ang0
+    return C1 * np.cos(t*omega) - C2*omega * np.sin(t*omega)
 
 def step(ang_prev, ang_vel_prev):
     k1 = h * f1(ang_prev, ang_vel_prev)
@@ -30,14 +42,34 @@ def step(ang_prev, ang_vel_prev):
 
     return ang, ang_vel 
 
-X = [0]
-Y = [8]
 
+X = [ang0]
+Y = [ang_vel0]
 for i in range(1, 200):
     x, y = step(X[-1], Y[-1])
     X.append(x)
     Y.append(y)
+    
+T = [0]  
+Ang = [ang0]
+Ang_vel = [ang_vel0]
+Simp_sin = [ang0]
+DSimp_sin = [ang_vel0]
+for i in range(1, int(t_r/h)):
+    x, y = step(Ang[-1], Ang_vel[-1])
+    T.append(i * h)
+    Ang.append(x)
+    Ang_vel.append(y)
+    Simp_sin.append(simp_sin(T[-1]))
+    DSimp_sin.append(dsimp_sin(T[-1]))
 
-plt.plot(X, Y)
+
+    
+    
+plt.plot(Simp_sin, DSimp_sin, 'r')
+plt.plot(X, Y,'k')
 plt.grid(True)
+plt.xlim([-5, 5])
+plt.ylim([7, 9])
+# plt.axis('equal')
 plt.show()
