20ee8b2e9283c3ce467037200b22dd887683a94a
Alexander Rodionov <alx1dr@gmail.com>
2016-10-11 17:18:53 +0300

    String numeric solution
    working shit

--- mat-model-string.py
+++ mat-model-string.py
@@ -1,57 +1,74 @@
-import matplotlib
-matplotlib.use('TKAgg')
+# macOS only ############
+import matplotlib       #
+matplotlib.use('TKAgg') #
+#########################
 
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
 
-N = 12
-a = 2
-l = 3
-pl = np.pi / l
-h = 0.05
+# mb add some ascii style schematic
+# solution outline.
+# Dlya naglyadnosti
+# here in comments
+# or on github (pics, .md, etc)
 
 
-def InitPos(x):
+l = 3. # length of the string
+n = 40 # number of length chunks
+# h = 0.1
+h = l/n # string step
+a = 3. # a^2 = N/ro - sorta tension coefficient
+b = 100 # seconds
+m = 20000 # number of time chunks
+k = b / m # chunk of time in seconds
+r = a * k / h
+
+def initial_state(x):
     if x == 0 or x == l: return 0
     return np.cos(- (x - 3) ** 2)
 
 def InitSpeed(x):
     return 0
 
-def A(n):
-    a = lambda x, n: InitPos(x) * np.sin(pl * n * x)
-    sum_a = 0
-    for i in range(int(l / h)):
-        sum_a += a(i * h, n) * h
-    return sum_a * 2 / l
-
-def B(n):
-    b = lambda x, n: InitSpeed(x) * np.sin(pl * n * x)
-    sum_b = 0
-    for i in range(int(l / h)):
-        sum_b += b(i * h, n) * h
-    return sum_b * 2 / l
 
 
-def U(x, t):
-    ser = [(A(i) * np.cos(pl * i * a * t) + B(i) * np.sin(pl * i * a * t)) * np.sin(pl * i * x) for i in range(1, N)]
-    return sum(ser)
 
 
+
+def u_curr(U_prev, U_pprev):
+    U_curr = np.zeros(n)
+    for i in range(1, n - 1):
+        U_curr[i] = 2 * (1 - r**2) * U_prev[i] + (U_prev[i+1] + U_prev[i-1]) * r**2 - U_pprev[i]
+    return U_curr
+
+
+
+# Draw
 fig, ax = plt.subplots()
 
-X = np.arange(0.0, l, h)
-line, = ax.plot(X, U(X, 0))
+X = np.linspace(0, l, n)
 
-def animate(i):
-    line.set_ydata(U(X, i/50))  # update the data
+U_pprev = [initial_state(s) for s in X]
+U_prev = U_pprev # TODO add += speed
+
+line, = ax.plot(X, U_prev)
+
+
+
+def animate(frame_number):
+    global U_prev, U_pprev
+    U_curr = u_curr(U_prev, U_pprev)
+    line.set_ydata(U_curr)  # update the data
+    U_pprev = U_prev
+    U_prev = U_curr
+    # print(frame_number)
     return line,
 
 def init():
-    line.set_ydata(np.ma.array(X, mask=True))
+    line.set_ydata(U_prev)
     return line,
 
-ani = animation.FuncAnimation(fig, animate, np.arange(1, 2000), init_func=init, interval=1, blit=True)
-ax.set_ylim([-1,1])
+ani = animation.FuncAnimation(fig, animate, init_func=init, interval=k*1000, blit=True)
+ax.set_ylim([-2,2])
 plt.show()
