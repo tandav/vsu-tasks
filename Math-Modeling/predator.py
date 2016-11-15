49ccf92621de37432e82b501363edeae08293d7e
Alexander Rodionov <alx1dr@gmail.com>
2016-11-15 22:59:24 +0300

    Update predator.py

--- Math-Modeling/predator.py
+++ Math-Modeling/predator.py
@@ -1,34 +1,62 @@
 from scipy.integrate import odeint
 from numpy import *
 from matplotlib.pyplot import *
+# import matplotlib.pyplot as plt
+
+
+r1 = 1
+r2 = 2.1
+a1 = 1.5
+a2 = 1.2
+b1 = 0.3
+b2 = 0.3
+
+# r1 = 1
+# r2 = 2.1
+# a1 = 1.5
+# a2 = 1.2
+# b1 = 0.3
+# b2 = 0.3
 
 def LotkaVolterra(state,t):
-	alpha = 0.01
-	beta =  1.1
-	sigma = 0.5
-	gamma = 2.1
-	x = state[0]
-	y = state[1]
-	xd = x*(alpha - beta*y)
-	yd = -y*(gamma - sigma*x)
-	return [xd,yd]
+	n1 = state[0]
+	n2 = state[1]
+	n1d = n1 * (r1 - b1*n1 - a2*n2)
+	n2d = n2 * (r2 - b2*n2 - a1*n1)
+	return [n1d, n2d]
 
 
-t = arange(0, 500, 1)
-state0 = [0.5, 0.5]
+
+t = arange(0, 30, 0.01)
+state0 = [2, 1]
 
 state = odeint(LotkaVolterra, state0, t)
-# figure()
 
 x = [e[0] for e in state]
 y = [e[1] for e in state]
-
+figure()
 plot(t, state)
-# plot(x, y)
-# ylim([0,8])
+grid(True)
+title('Lotka-Volterra equations')
 xlabel('Time')
 ylabel('Population Size')
-legend(('x (prey)','y (predator)'))
-title('Lotka-Volterra equations')
+
+
+figure() 
+plot(x, y)
+
+lx = [r2/a1, r1/b1]
+ly = [r2/b2, r1/a2]
+
+# plot([r2/a1, r2/b2, 'ro-')
+# plot([r2/a1, r1/b1], 'ro-')
+# plot([0, r1/a2, 'ro-')
 grid(True)
+title('Phase Portrait')
+axis('equal')
+xlabel('N1')
+ylabel('N2')
+
+# ylim([0,8])
+# legend(('x (prey)','y (predator)'))
 show()
