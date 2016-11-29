98c6b01e68d3452dc1b8a1234014e6c4255149d8
Alexander Rodionov <alx1dr@gmail.com>
2016-11-29 19:26:58 +0300

    add LaTeX

--- Math-Modeling/logistic-map.py
+++ Math-Modeling/logistic-map.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy
 
-rs = numpy.linspace(2.8, 4, 1000)
+rs = numpy.linspace(2.8, 4, 4000)
 r_xxx = []
 
 for r in rs:
@@ -17,9 +17,11 @@ r_xxx = numpy.array(r_xxx)
 plt.plot(r_xxx[:, 0], r_xxx[:, 1], ',') # This draws a scatter plot of (r, x) points.
 
 # draw
-plt.title('Logistic Map', fontsize=18)
-plt.xlabel('r', fontsize=16)
-plt.ylabel('x', fontsize=16)
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.title("Logistic Map: " "$x_{n+1} = rx_{n}(1-x_{n})", fontsize=18)
+plt.xlabel('$r', fontsize=20)
+plt.ylabel('$x', fontsize=20, rotation='horizontal')
 plt.grid(True)
-plt.show()
-
+plt.savefig('logistic-map.png', dpi=200)
+# plt.show()
