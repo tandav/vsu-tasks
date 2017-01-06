915138583f90e9fa26e12f86ecc819160c1252d6
Alexander Rodionov <alx1dr@gmail.com>
2017-01-06 18:38:47 +0300

    add scipy.integrate.ode solver

--- Math-Modeling/untitled/holing.py
+++ Math-Modeling/untitled/holing.py
@@ -1,74 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.integrate import ode
+from scipy.integrate import odeint
 
 t_end = 200
-
+t = np.linspace(0, 1, 51)
 h = 0.01
 
-x0, y0 = 20, 56
+x1_0 = 20
+x2_0 = 56
+
 
 r, K, w, D = 1, 1.5, 1, 1
 
 S, J = .2, .5
 
-def dx1(x1, x2):
-    return r * (1 - x1 / K) * x1 - w * x1 * x2 / (D + x1)
 
+def dX_dt(X, t=0):
+    """ Return the growth rate of x1 and x2 populations. """
+    return np.array([ r * (1 - X[0] / K) * X[0] - w * X[0] * X[1] / (D + X[0]),
+                      S * (1 - J * X[1] / X[0]) * X[1] ])
 
-def dx2(x1, x2):
-    return S * (1 - J * x2 / x1) * x2
-
-
-def step(x_prev, y_prev):
-    k1 = h * dx1(x_prev, y_prev)
-    l1 = h * dx2(x_prev, y_prev)
-
-    k2 = h * dx1(x_prev + k1 / 2, y_prev + l1 / 2)
-    l2 = h * dx2(x_prev + k1 / 2, y_prev + l1 / 2)
-
-    k3 = h * dx1(x_prev + k2 / 2, y_prev + l2 / 2)
-    l3 = h * dx2(x_prev + k2 / 2, y_prev + l2 / 2)
-
-    k4 = h * dx1(x_prev + k3, y_prev + l3)
-    l4 = h * dx2(x_prev + k3, y_prev + l3)
-
-    x_ret = x_prev + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-    y_ret = y_prev + (l1 + 2 * l2 + 2 * l3 + l4) / 6
-
-    return x_ret, y_ret
-
-
-T = [0]
-X1 = [x0]
-X2 = [y0]
-
-for i in range(1, int(t_end / h)):
-    x1, x2 = step(X1[-1], X2[-1])
-    T.append(i * h)
-    X1.append(x1)
-    X2.append(x2)
+t = np.linspace(0, 15,  1000)
+X0 = np.array([10, 5])
+X = odeint(dX_dt, X0, t)
 
 f, (ax1, ax2) = plt.subplots(1, 2)
 
-a, bet = D / K, S / r
-
-cond1 = a >= 1 or a < 1 and a + bet >= 1
-
-cond2 = a + bet < 1 and (1 - a - bet) ** 2 - 8 * a * bet <= 0
-
-print(a, bet)
-
-print(cond1, cond2)
+x1, x2 = X.T
 
 
-ax1.plot(X1, X2)
+ax1.plot(x1, x2)
 ax1.set_title("Phase")
 ax1.grid()
 
-ax2.plot(T, X2)
-ax2.plot(T, X1, 'r')
-ax2.set_title("Red - victim")
+ax2.plot(t, x1, 'b')
+ax2.plot(t, x2, 'r')
+ax2.set_title("fdfd")
 ax2.grid()
 
 plt.show()
--- Math-Modeling/untitled/odeint-test.py
+++ Math-Modeling/untitled/odeint-test.py
@@ -3,11 +3,8 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 
-def fun(y, t, a):
-    """Define the right-hand side of equation dy/dt = a*y"""
-    f = a * y
-    return f
-
+def dy(y, t):
+    return -2.5 * y
 
 # Initial condition
 y0 = 100.0
@@ -15,16 +12,14 @@ y0 = 100.0
 # Times at which the solution is to be computed.
 t = np.linspace(0, 1, 51)
 
-# Parameter value to use in `fun`.
-a = -2.5
-
 # Solve the equation.
-y = odeint(fun, y0, t, args=(a,))
-
+y = odeint(dy, y0, t)
+print(y)
+print(len(y))
 # Plot the solution.  `odeint` is generally used to solve a system
 # of equations, so it returns an array with shape (len(t), len(y0)).
 # In this case, len(y0) is 1, so y[:,0] gives us the solution.
-plt.plot(t, y[:,0])
+plt.plot(t, y[:, 0])
 plt.xlabel('t')
 plt.ylabel('y')
 plt.show()
