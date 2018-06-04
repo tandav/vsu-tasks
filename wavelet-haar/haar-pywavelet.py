13a12e2ed2f3ab0460d63dc8274193aa160db0cc
Alexander Rodionov <alx1dr@gmail.com>
2018-06-04 12:40:03 +0300

    haar wavelets compression project

--- wavelet-haar/haar-pywavelet.py
+++ wavelet-haar/haar-pywavelet.py
@@ -1,22 +1,57 @@
 import numpy as np
 import pywt
-import matplotlib
-# matplotlib.rcParams['text.usetex'] = True
-# matplotlib.rcParams['text.latex.unicode'] = True
 import matplotlib.pyplot as plt
 
-# N = 128
+N = 128
 # N = 256
-N = 1024
-x = np.arange(0, 0.1 * N, 0.1)
+# N = 1024
+x = np.linspace(0, 0.1 * N, N, endpoint=False)
 f = np.sin(x) + 0.15 * np.sin(20 * x)
 
 
-cA, cD = pywt.dwt(f, 'Haar')
-print(np.min(cA), np.max(cA), np.min(np.abs(cA)))
-print(np.min(cD), np.max(cD), np.min(np.abs(cD)))
+def rmse(original, measured):
+    return np.sqrt(np.sum(np.square(original - measured)) / len(original))
 
-threshold = 0 # bigger threshold -> more filtered wavelets, compression percentage is smaller
+def nrmse(original, measured):
+    return np.sqrt(np.sum(np.square(original - measured)) / len(original) / np.mean(original))
+
+def mae(original, measured):
+    return np.mean(np.abs(original - measured))
+
+
+cA, cD = pywt.dwt(f, 'Haar')
+# print(np.min(cA), np.max(cA), np.min(np.abs(cA)))
+# print(np.min(cD), np.max(cD), np.min(np.abs(cD)))
+
+
+# nt = 512
+# thresholds   = np.linspace(0, np.max(cA), nt)
+# compressions = np.empty_like(thresholds)
+# rmse_s       = np.empty_like(thresholds)
+# nrmse_s      = np.empty_like(thresholds)
+# mae_s        = np.empty_like(thresholds)
+
+# for i, threshold in enumerate(thresholds):
+#     print(f'{i}/{nt}')
+#     cA_temp = np.copy(cA)
+#     cD_temp = np.copy(cD)
+
+#     cA_temp[np.abs(cA_temp) < threshold] = 0
+#     cD_temp[np.abs(cD_temp) < threshold] = 0
+    
+#     filtered_wavelets = np.count_nonzero(np.abs(cA_temp) < threshold) + np.count_nonzero(np.abs(cD_temp) < threshold)
+#     compressions[i] = filtered_wavelets / N * 100
+
+#     h = pywt.idwt(cA_temp, cD_temp, 'Haar')
+    
+#     rmse_s[i]  =  rmse(f, h)
+#     nrmse_s[i] = nrmse(f, h)
+#     mae_s[i]   =   mae(f, h)
+
+
+
+
+threshold = 0.3 # bigger threshold -> more filtered wavelets, compression percentage is smaller
 cA[np.abs(cA) < threshold] = 0
 cD[np.abs(cD) < threshold] = 0
 filtered_wavelets = np.count_nonzero(np.abs(cA) < threshold) + np.count_nonzero(np.abs(cD) < threshold)
@@ -25,36 +60,26 @@ filtered_wavelets = np.count_nonzero(np.abs(cA) < threshold) + np.count_nonzero(
 h = pywt.idwt(cA, cD, 'Haar')
 
 
-def rmse(original, measured):
-    return np.sqrt(np.sum(np.square(original - measured)) / N)
 
-def nrmse(original, measured):
-    return np.sqrt(np.sum(np.square(original - measured)) / N / np.mean(original))
-
-def mae(original, measured):
-    return np.mean(np.abs(original - measured))
 
 rmse   =  rmse(f, h)
 nrmse  = nrmse(f, h)
 mae    =   mae(f, h)
-# nrmse = rmse(np.array([-100, 20480000, 30]), np.array([1, 2, 3]))
 
-# NRMSD = nrmsd(h, f)
 
-# RMSD = np.sqrt(np.sum(np.square(f - h)) / N)
 compression = filtered_wavelets / N * 100
-size = (1 - filtered_wavelets / N) * 100
+# size = (1 - filtered_wavelets / N) * 100
 
-print(
-f'''
-threshold\t{threshold}
-filtered wavelets:\t{filtered_wavelets}/{N}
-compression:\t{compression}%
-size:\t{size}% of original
-RMSE:\t{rmse}
-NRMSE:\t{nrmse}
-MAE:\t{mae}
-''')
+# print(
+# f'''
+# threshold\t{threshold}
+# filtered wavelets:\t{filtered_wavelets}/{N}
+# compression:\t{compression}%
+# size:\t{size}% of original
+# RMSE:\t{rmse}
+# NRMSE:\t{nrmse}
+# MAE:\t{mae}
+# ''')
 
 
 
@@ -62,9 +87,28 @@ MAE:\t{mae}
 # print(np.var(f), np.var(h))
 
 # plt.title(f'{N} отсчетов, порог {threshold}, компрессия {compression}')
-# plt.xlabel('x')
+plt.xlabel(f'порог {threshold}        компрессия {compression:.2f}%        RMSE={rmse:.2f}        MAE={mae:.2f}')
 plt.plot(x, f, 'k:', linewidth=1,    label='исходная f(x)')
 plt.plot(x, h, 'b-', linewidth=0.8,  label='восстановленная')
-plt.legend(loc=1)
+
+
+fig = plt.gcf()
+
+fig.set_size_inches(19 * 0.39, 4.7 * 0.39) # 1cm = 0.39inch
+
+# plt.plot(compressions,  rmse_s, 'b-', linewidth=0.8,  label='RMSE')
+# plt.plot(compressions, nrmse_s, 'r-', linewidth=0.8,  label='NRMSE')
+# plt.plot(compressions,   mae_s, 'g--', linewidth=0.8,  label='MAE')
+# plt.xlabel('процент компрессии')
+# plt.xlim(xmin=0)
+# plt.ylim(ymin=0)
+
+plt.legend(loc=0)
+
 plt.grid(color='0.6', linestyle=':', linewidth=0.5)
+# plt.tight_layout()
+plt.subplots_adjust(left=0.05, right=0.98, top=0.95, bottom=0.25)
+
+fig.savefig(f'images/signals-{threshold}.png', dpi=100)
+
 plt.show()
